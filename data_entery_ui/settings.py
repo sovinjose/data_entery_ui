a3caddd660c52e71342885c82788a4ed8ce4c5de
jibin jose <jibinjosepez@gmail.com>
2017-05-21 01:17:27 +0530

    fghj

--- data_entery_ui/settings.py
+++ data_entery_ui/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+print '%s/server-ca.pem' % (BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -80,8 +80,8 @@ DATABASES = {
         'NAME': 'wtj',
         'USER': 'root',
         'PASSWORD': 'worktojob',
-        'HOST': '104.199.102.4',   # Or an IP Address that your DB is hosted on
-    }
+        'HOST': '104.199.102.4',   # Or an IP Address that your DB is hosted on,
+        }
 }
 
 # Password validation
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
