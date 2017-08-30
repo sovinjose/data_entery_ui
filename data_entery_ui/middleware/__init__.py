84a812be139d76b0cb70f692f81797b81651466b
jibin jose <jibinjosepez@gmail.com>
2017-08-31 00:24:45 +0530

    df

--- data_entery_ui/middleware/__init__.py
+++ data_entery_ui/middleware/__init__.py
@@ -1,16 +1,15 @@
+
 import re
 import logging
 from django import http
 from django.core.cache import cache
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import AnonymousUser
+from django.shortcuts import render
 
 
 class HandleRequests(object):
-    def process_view(self, request, *arg, **kwargs):
-        if not re.match(SAFE_TO_REDIRECT_URI_REGEX, request.path):
-            if not request.user.is_authenticated() or isinstance(request.user, AnonymousUser):
-                return http.HttpResponseRedirect('/login?next=%s' % request.path)
-            return None
-SAFE_TO_REDIRECT_URI_REGEX = '(/accounts/login/)|(/admin/)|(/logout)'
-
+    def process_request(self, request):
+        if not request.user.is_authenticated() and not re.match(SAFE_TO_REDIRECT_URI_REGEX, request.path):
+            logging.info('User not logged in. Going to redirect to Login page.')
+            return http.HttpResponseRedirect('/accounts/login/?next=%s' % request.path)
+        return None
+SAFE_TO_REDIRECT_URI_REGEX = '(/accounts/login/)|(/admin/)|(/register)'
