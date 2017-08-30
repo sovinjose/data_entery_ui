8084b6d0d9d910a3c0e27e21cf62097d5b241637
jibin jose <jibinjosepez@gmail.com>
2017-08-31 00:01:46 +0530

    sdfg

--- data_entery_ui/middleware/__init__.py
+++ data_entery_ui/middleware/__init__.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import AnonymousUser
 
 
 class HandleRequests(object):
-    def process_view(self, request, view_func, view_args, view_kwargs):
+    def process_view(self, request, *arg, **kwargs):
         if not re.match(SAFE_TO_REDIRECT_URI_REGEX, request.path):
             if not request.user.is_authenticated() and not isinstance(request.user, AnonymousUser):
                 return http.HttpResponseRedirect('/login?next=%s' % request.path)
